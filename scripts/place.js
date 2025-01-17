46e07e95af74135075254e7609c8a0a78e7e2031
Frankroz <franjrm201518@hotmail.com>
2025-01-18 00:09:14 +0800

    place/weather app created

---
+++ scripts/place.js
@@ -0,0 +1,396 @@
+// Fetch the data from the API
+async function fetchWeatherData() {
+  const latitude = 4;
+  const longitude = -73.25;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,is_day,weather_code,wind_speed_10m`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return null;
+  }
+}
+
+// WMO codes
+const wmo_codes_icons = {
+  0: {
+    day: {
+      description: "Sunny",
+      image: "http://openweathermap.org/img/wn/01d@2x.png",
+    },
+    night: {
+      description: "Clear",
+      image: "http://openweathermap.org/img/wn/01n@2x.png",
+    },
+  },
+  1: {
+    day: {
+      description: "Mainly Sunny",
+      image: "http://openweathermap.org/img/wn/01d@2x.png",
+    },
+    night: {
+      description: "Mainly Clear",
+      image: "http://openweathermap.org/img/wn/01n@2x.png",
+    },
+  },
+  2: {
+    day: {
+      description: "Partly Cloudy",
+      image: "http://openweathermap.org/img/wn/02d@2x.png",
+    },
+    night: {
+      description: "Partly Cloudy",
+      image: "http://openweathermap.org/img/wn/02n@2x.png",
+    },
+  },
+  3: {
+    day: {
+      description: "Cloudy",
+      image: "http://openweathermap.org/img/wn/03d@2x.png",
+    },
+    night: {
+      description: "Cloudy",
+      image: "http://openweathermap.org/img/wn/03n@2x.png",
+    },
+  },
+  45: {
+    day: {
+      description: "Foggy",
+      image: "http://openweathermap.org/img/wn/50d@2x.png",
+    },
+    night: {
+      description: "Foggy",
+      image: "http://openweathermap.org/img/wn/50n@2x.png",
+    },
+  },
+  48: {
+    day: {
+      description: "Rime Fog",
+      image: "http://openweathermap.org/img/wn/50d@2x.png",
+    },
+    night: {
+      description: "Rime Fog",
+      image: "http://openweathermap.org/img/wn/50n@2x.png",
+    },
+  },
+  51: {
+    day: {
+      description: "Light Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Light Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  53: {
+    day: {
+      description: "Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  55: {
+    day: {
+      description: "Heavy Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Heavy Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  56: {
+    day: {
+      description: "Light Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Light Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  57: {
+    day: {
+      description: "Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Freezing Drizzle",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  61: {
+    day: {
+      description: "Light Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Light Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  63: {
+    day: {
+      description: "Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  65: {
+    day: {
+      description: "Heavy Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Heavy Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  66: {
+    day: {
+      description: "Light Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Light Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  67: {
+    day: {
+      description: "Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10d@2x.png",
+    },
+    night: {
+      description: "Freezing Rain",
+      image: "http://openweathermap.org/img/wn/10n@2x.png",
+    },
+  },
+  71: {
+    day: {
+      description: "Light Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Light Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  73: {
+    day: {
+      description: "Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  75: {
+    day: {
+      description: "Heavy Snow",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Heavy Snow",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  77: {
+    day: {
+      description: "Snow Grains",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow Grains",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  80: {
+    day: {
+      description: "Light Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Light Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  81: {
+    day: {
+      description: "Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  82: {
+    day: {
+      description: "Heavy Showers",
+      image: "http://openweathermap.org/img/wn/09d@2x.png",
+    },
+    night: {
+      description: "Heavy Showers",
+      image: "http://openweathermap.org/img/wn/09n@2x.png",
+    },
+  },
+  85: {
+    day: {
+      description: "Light Snow Showers",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Light Snow Showers",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  86: {
+    day: {
+      description: "Snow Showers",
+      image: "http://openweathermap.org/img/wn/13d@2x.png",
+    },
+    night: {
+      description: "Snow Showers",
+      image: "http://openweathermap.org/img/wn/13n@2x.png",
+    },
+  },
+  95: {
+    day: {
+      description: "Thunderstorm",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Thunderstorm",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+  96: {
+    day: {
+      description: "Light Thunderstorms With Hail",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Light Thunderstorms With Hail",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+  99: {
+    day: {
+      description: "Thunderstorm With Hail",
+      image: "http://openweathermap.org/img/wn/11d@2x.png",
+    },
+    night: {
+      description: "Thunderstorm With Hail",
+      image: "http://openweathermap.org/img/wn/11n@2x.png",
+    },
+  },
+};
+
+// Function to calculate windchill in Celsius
+function calculateWindChillCelsius(temperature, windSpeed) {
+  if (temperature <= 10 && windSpeed > 4.8) {
+    return (
+      13.12 +
+      0.6215 * temperature -
+      11.37 * Math.pow(windSpeed, 0.16) +
+      0.3965 * temperature * Math.pow(windSpeed, 0.16)
+    );
+  } else {
+    return "N/A";
+  }
+}
+
+// Function to calculate windchill in Fahrenheit
+function calculateWindChillFahrenheit(temperature, windSpeed) {
+  if (temperature <= 50 && windSpeed > 3) {
+    return (
+      35.74 +
+      0.6215 * temperature -
+      35.75 * Math.pow(windSpeed, 0.16) +
+      0.4275 * temperature * Math.pow(windSpeed, 0.16)
+    );
+  } else {
+    return "N/A";
+  }
+}
+
+// Check the weather conditions
+async function updateWeather() {
+  const weatherData = await fetchWeatherData();
+  if (weatherData) {
+    const temperature = 3;
+    const windSpeed = weatherData.current.wind_speed_10m;
+    const wmo_code = weatherData.current.weather_code;
+    const is_day = weatherData.current.is_day;
+    const wind = weatherData.current.wind_speed_10m;
+
+    // Function to calculate windchill in Celsius
+    function calculateWindChillCelsius(temperature, windSpeed) {
+      if (temperature <= 10 && windSpeed > 4.8) {
+        return (
+          13.12 +
+          0.6215 * temperature -
+          11.37 * Math.pow(windSpeed, 0.16) +
+          0.3965 * temperature * Math.pow(windSpeed, 0.16)
+        );
+      } else {
+        return "N/A";
+      }
+    }
+
+    // Function to calculate windchill in Fahrenheit
+    function calculateWindChillFahrenheit(temperature, windSpeed) {
+      if (temperature <= 50 && windSpeed > 3) {
+        return (
+          35.74 +
+          0.6215 * temperature -
+          35.75 * Math.pow(windSpeed, 0.16) +
+          0.4275 * temperature * Math.pow(windSpeed, 0.16)
+        );
+      } else {
+        return "N/A";
+      }
+    }
+
+    // Determine the unit based on location (replace with actual location logic)
+    const location = "United States"; // Example: Assuming the location is the United States (Fahrenheit)
+
+    // Calculate windchill based on location
+    let windChill;
+    if (location === "United States") {
+      windChill = calculateWindChillFahrenheit(
+        temperature * 1.8 + 32,
+        windSpeed * 0.621371
+      ); // Convert to Fahrenheit
+    } else {
+      windChill = calculateWindChillCelsius(temperature, windSpeed);
+    }
+
+    document.getElementById("weather_icon_img").src =
+      wmo_codes_icons[wmo_code][is_day ? "day" : "night"].image;
+    document.getElementById("weatherInfo").innerHTML = `
+          <p><strong>Temperature:</strong> ${temperature.toFixed(1)}°C</p>
+          <p><strong>Conditions:</strong> ${
+            wmo_codes_icons[wmo_code][is_day ? "day" : "night"].description
+          }</p>
+          <p><strong>Wind:</strong> ${wind} km/h</p>
+          <p><strong>Wind Chill:</strong> ${
+            isNaN(windChill) ? windChill : windChill.toFixed(1) + "°C"
+          }</p>
+      `;
+  }
+}
+
+updateWeather();
